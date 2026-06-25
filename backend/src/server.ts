6f365dae291c5081139281eabdb9d726cdfc3739
sanjograiofficial <sanjograiofficial@gmail.com>
2026-06-25 14:42:47 +0545

    Feat: asynchandler, errorhandler, routes, controllers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,23 @@
 import express from "express";
 import { log } from "node:console";
 import "dotenv/config";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import routes from "./routes/routes.js";
+import cors from "cors";
 
 let port = process.env.PORT;
+let frontendPort = process.env.FRONTENDPORT;
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: `https:localhost:${frontendPort}`,
+  }),
+);
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+app.use("/", routes);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   log(`server started at: http://localhost:${port}`);
