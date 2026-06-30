2b27d56e80173f221817422e238fdffbef5a6f31
sanjograiofficial <sanjograiofficial@gmail.com>
2026-06-30 21:02:00 +0545

    Auth

--- backend/src/services/LeaderBoards.service.ts
+++ backend/src/services/LeaderBoards.service.ts
@@ -1,14 +1,14 @@
 import prisma from "../db/prisma.js";
 
 const fetchLeaderBoardsService = async () => {
-  return await prisma.users.findMany({
+  return await prisma.player.findMany({
     orderBy: { score: "desc" },
     take: 10,
   });
 };
 
 const createRecordService = async (name: string, score: number) => {
-  return await prisma.users.create({
+  return await prisma.player.create({
     data: {
       name,
       score,
