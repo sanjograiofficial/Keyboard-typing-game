b19409f72ee37b98c04508f7d82f39b38cf9dff3
sanjograiofficial <sanjograiofficial@gmail.com>
2026-07-21 11:21:18 +0545

    ui: made changes to leaderboards ui

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,12 +6,12 @@ import routes from "./routes/routes.js";
 import cors from "cors";
 
 let port = process.env.PORT;
-let frontendPort = process.env.FRONTEND_PORT;
+let frontendURL = process.env.FRONTEND_URL;
 const app = express();
 app.use(express.json());
 app.use(
   cors({
-    // origin: `http://localhost:${frontendPort}`,
+    // origin: `http://localhost:${frontendURL}`,
     origin: `https://typing-game-qver.vercel.app`,
   }),
 );
